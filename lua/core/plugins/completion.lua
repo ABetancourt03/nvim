09bcf9df427054c3bdfddbd71cc8fef153e8b395
ABetancourt03 <neutralzgamer@gmail.com>
2024-04-29 23:56:33 -0400

    Update completion config

--- lua/core/plugins/completion.lua
+++ lua/core/plugins/completion.lua
@@ -1,5 +1,12 @@
 return {
-	"hrsh7th/nvim-cmp",
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
+	},
 	"hrsh7th/cmp-nvim-lsp",
 	"L3MON4D3/LuaSnip",
 	"saadparwaiz1/cmp_luasnip",
@@ -8,4 +15,5 @@ return {
 	"williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
 	"neovim/nvim-lspconfig",
+	"onsails/lspkind.nvim",
 }
