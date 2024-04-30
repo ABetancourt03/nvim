9756bbf104d2553dfcac26d7428677a3f7e6f860
ABetancourt03 <neutralzgamer@gmail.com>
2024-04-30 11:00:07 -0400

    Add format on save from lsp

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -112,7 +112,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 		vim.keymap.set("n", "<space>f", function()
-			vim.lsp.buf.format({ async = true })
+			vim.lsp.buf.format({ async = false })
 		end, opts)
+		vim.api.nvim_create_augroup("AutoFormatting", {})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = {
+				"*.astro",
+			},
+			group = "AutoFormatting",
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
 	end,
 })
