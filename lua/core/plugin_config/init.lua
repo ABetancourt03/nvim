b56760b04393adcab4143dbf469082dc606e2540
ABetancourt03 <neutralzgamer@gmail.com>
2024-04-29 22:12:30 -0400

    Update lualine config

--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -1,5 +1,4 @@
 require("core.plugin_config.colorscheme")
-require("core.plugin_config.lualine")
 require("core.plugin_config.treesitter")
 require("core.plugin_config.telescope")
 require("core.plugin_config.vim-test")
