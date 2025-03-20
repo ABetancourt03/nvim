efea9f44ffce40b9fdeeda5d0a794585d8b28c0c
ABetancourt03 <angelob2207@gmail.com>
2025-03-20 19:39:47 -0400

    change colorscheme

--- lua/core/plugins/lualine.lua
+++ lua/core/plugins/lualine.lua
@@ -51,7 +51,7 @@ return {
 		-- configure lualine with modified theme
 		lualine.setup({
 			options = {
-				theme = "gruvbox",
+				theme = my_lualine_theme,
 			},
 			sections = {
 				lualine_x = {
