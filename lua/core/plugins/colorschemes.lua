52483f81990331bdfa76d5e3df216f4ca4b7032d
ABetancourt03 <angelob2207@gmail.com>
2025-03-20 20:14:04 -0400

    Add colorschemes and disable copilot custom colors

--- lua/core/plugins/colorschemes.lua
+++ lua/core/plugins/colorschemes.lua
@@ -1,6 +1,14 @@
 return {
 	-- "morhetz/gruvbox",
 	"ellisonleao/gruvbox.nvim",
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_material_enable_italic = true
+		end,
+	},
 	"dracula/vim",
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	{ "olimorris/onedarkpro.nvim", priority = 1000 },
@@ -15,4 +23,9 @@ return {
 		lazy = false,
 		priority = 1000,
 	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+	},
+	"samharju/synthweave.nvim",
 }
--- lua/core/plugins/lualine.lua
+++ lua/core/plugins/lualine.lua
@@ -51,7 +51,7 @@ return {
 		-- configure lualine with modified theme
 		lualine.setup({
 			options = {
-				theme = my_lualine_theme,
+				theme = "auto",
 			},
 			sections = {
 				lualine_x = {
