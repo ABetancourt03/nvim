4fd3e06c17c58cf32ab3858fbb072101f05b57b0
ABetancourt03 <angelob2207@gmail.com>
2026-07-11 15:00:02 -0400

    update lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,7 +7,8 @@ return {
       icons_enabled = true,
       theme = "auto",
       component_separators = "|",
-      section_separators = { left = "", right = "" },
+      -- section_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
     },
   },
 }
