da9cef3b3001ad02219c79852a68f64a971f0f81
ABetancourt03 <angelob2207@gmail.com>
2026-08-09 12:35:44 -0400

    update treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,9 +1,9 @@
 require('nvim-treesitter').setup {
-	ensure_installed = {"lua", "help", "javascript", "typescript"},
-	sync_install = false,
-	auto_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	}
+  ensure_installed = { "lua", "help", "javascript", "typescript", "tsx", "jsdoc", "html" },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  }
 }
