f9e2596c99b1607e23a83e9b0a01333f79970131
ABetancourt03 <angelob2207@gmail.com>
2026-08-20 01:27:47 -0400

    update config

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,5 +1,3 @@
--- vim.cmd.colorscheme("nord")
-
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
